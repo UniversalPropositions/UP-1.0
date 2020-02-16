05b980f1514e8e1379185e811e8b501da8367bcf
Alexandre Rademaker <arademaker@gmail.com>
2020-02-16 14:13:39 -0300

    cleaning code

--- UP_English-EWT/make.sh
+++ UP_English-EWT/make.sh
@@ -4,6 +4,6 @@ sbcl --load merge.lisp --eval "(in-package :merge-pb)" --eval "(main)" --eval "(
 for f in en_ewt-up-{dev,test,train}.conllu.new; do
     awk -f conllu-to-conll.awk $f > $(basename $f .conllu.new).conllu;
 done
-# rm en_ewt-up-{dev,test,train}.conllu.new
+rm en_ewt-up-{dev,test,train}.conllu.new
 
 
